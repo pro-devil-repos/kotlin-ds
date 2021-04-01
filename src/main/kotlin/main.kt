3466c8a1f00176963a7f112852beba1e6b060bc7
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2021-04-01 14:32:39 +0300

    Убрал структуры данных в другую репу, здесь будут пробы по дата сайенсу потом

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,4 @@
-import pro.devil.collections_ex.linkedList.*
+
 
 fun main() {
-    val list = LinkedList<Int>()
-    val list2 = LinkedList<Int>()
-
-    list.append(1).append(2).append(3)
-    list2.append(1).append(2).append(3).append(4)
-
-    println(list.middleItem())
-    println(list2.middleItem())
-
-    list.printReversed()
-
-    println(list)
-    println(list.reversedList())
-
-
 }
